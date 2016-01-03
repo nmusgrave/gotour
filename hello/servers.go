a95d8db5ee2718753d72b99523c9069f7214473c
nmusgrave <naomi.g.musgrave@gmail.com>
2016-01-02 20:46:51 -0800

    reorg files

--- hello/servers.go
+++ hello/servers.go
@@ -2,8 +2,24 @@ package main
 
 import (
   "fmt"
+  "log"
+  "net/http"
 )
+
+type Hello struct {}
+
+// Implements the Handler interface
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "hello")
+}
 
 func servers() {
   fmt.Println("* Servers *")
+  fmt.Println("HTTP package serves requests using any value impl Handler")
+
+  var h Hello
+  err := http.ListenAndServe("localhost:4000", h)
+  if err := nil {
+    log.Fatal(err)
+  }
 }
